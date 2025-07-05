bbd06f3ea383afdc21705a1837b5cc7e560b0ddc
Sr1jan27 <singhsrijan809@gmail.com>
2025-07-05 19:26:01 +0530

    loop

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 prompt = "Enter a todo:"
-user_text = input(prompt)
+todos = []
+i =0
+while i<5:
+    user_text = input(prompt)
+    todos.append(user_text)
+    i = i + 1
+# user_text = input(prompt)
 print("todo's are: - ")
-print(user_text)
+print(todos)
