381e49156494da3a7e7602bb9dd14270603f3b48
Sr1jan27 <singhsrijan809@gmail.com>
2025-07-11 13:15:53 +0530

    editing tood

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add or show or exit: ")
+    user_action = input("Type add or show or edit or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -11,7 +11,17 @@ while True:
         case 'show':
             for item in todos:
                 print(item)
+        case 'edit':
+            index = int(input("Tell us which item you want to edit :- "))
+            if(index> len(todos)):
+                print("Enter Correct position dude")
+            else :
+                todos[index-1] = input("Please enter the edited todo:- ")
+            # todo = input("Please enter the edited todo:- ")
+            # todos[index-1] = todo
         case 'exit':
             break
+        case whatever:
+            print("Wrong Command dude!")
 
 print('Bye!')
